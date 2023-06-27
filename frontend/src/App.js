3d59cd10cb6a41842c2b4a02b9aec984549d2a07
DuanLeeWatkins <duanthedev@gmail.com>
2023-06-27 17:39:01 -0400

    added orb component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
+import styled from "styled-components";
+import bg from "./images/bg.png";
+import {MainLayout} from "./styles/Layouts"
+import Orb from "./components/Orb/Orb";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <AppStyled bg={bg} className="App">
+      <Orb />
+      <MainLayout>
+        
+      </MainLayout>
+    </AppStyled>
   );
 }
+
+const AppStyled = styled.div`
+  height: 100vh;
+  background-image: url(${(props) => props.bg});
+  position: relative;
+`;
 
 export default App;
